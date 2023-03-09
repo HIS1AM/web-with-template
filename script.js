41fd76c9dde400166d66956fb5cabfdbf27a95c4
HIS1AM <112554334+HIS1AM@users.noreply.github.com>
2023-03-09 14:40:35 +0100

    Website

---
+++ script.js
@@ -0,0 +1,7 @@
+const iconBtn = document.querySelector('.icon_btn')
+const iconBtnIcon = document.querySelector('.icon_btn i')
+const dropDowm = document.querySelector('.dropdowm')
+
+iconBtn.onclick = function () {
+  dropDown.classList.toggle('open')
+}
